6dede29a4edc21981bedb83910b2f16bbac80b0b
Your Namepythonvista <you@example.comnablintech@gmail.com>
2022-08-14 18:11:50 -0700

    updated

--- src/components/Admin/AllBusiness.vue
+++ src/components/Admin/AllBusiness.vue
@@ -1,76 +1,30 @@
 <template>
   <div class="main-panel ">
     <div class="content-wrapper">
-       <div class="row">
+      <div class="row">
         <div class="col-md-12 grid-margin stretch-card">
           <div class="card">
             <div class="card-body">
-              <p class="card-title mb-0">Top Products</p>
+              <p class="card-title mb-0">All Company</p>
               <div class="table-responsive">
                 <table class="table table-striped table-borderless">
                   <thead>
                     <tr>
-                      <th>Product</th>
-                      <th>Price</th>
-                      <th>Date</th>
-                      <th>Status</th>
+                      <th>Business Name</th>
+                      <th>Business Sector</th>
+                      <th>Business Email</th>
+                      <th>Registration Status</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>Search Engine Marketing</td>
-                      <td class="font-weight-bold">$362</td>
-                      <td>21 Sep 2018</td>
+                    <tr v-for="(company, index) in allCompany" :key="index">
+                      <td>{{ company.companyName }}</td>
+                      <td class="font-weight-bold">{{ getAocBusiness(company.id).businessSector }}</td>
+                      <td>{{ company.companyEmail }}</td>
                       <td class="font-weight-medium">
-                        <div class="badge badge-success">Completed</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Search Engine Optimization</td>
-                      <td class="font-weight-bold">$116</td>
-                      <td>13 Jun 2018</td>
-                      <td class="font-weight-medium">
-                        <div class="badge badge-success">Completed</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Display Advertising</td>
-                      <td class="font-weight-bold">$551</td>
-                      <td>28 Sep 2018</td>
-                      <td class="font-weight-medium">
-                        <div class="badge badge-warning">Pending</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Pay Per Click Advertising</td>
-                      <td class="font-weight-bold">$523</td>
-                      <td>30 Jun 2018</td>
-                      <td class="font-weight-medium">
-                        <div class="badge badge-warning">Pending</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>E-Mail Marketing</td>
-                      <td class="font-weight-bold">$781</td>
-                      <td>01 Nov 2018</td>
-                      <td class="font-weight-medium">
-                        <div class="badge badge-danger">Cancelled</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Referral Marketing</td>
-                      <td class="font-weight-bold">$283</td>
-                      <td>20 Mar 2018</td>
-                      <td class="font-weight-medium">
-                        <div class="badge badge-warning">Pending</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Social media marketing</td>
-                      <td class="font-weight-bold">$897</td>
-                      <td>26 Oct 2018</td>
-                      <td class="font-weight-medium">
-                        <div class="badge badge-success">Completed</div>
+                        <div class="badge " :class="registrationBadge(company.businessRegistration, company.founderRegistration).badge">{{
+                            registrationBadge(company.businessRegistration, company.founderRegistration).status
+                        }}</div>
                       </td>
                     </tr>
                   </tbody>
@@ -86,7 +40,73 @@
 
 <script>
 export default {
-  name: "AllBusiness"
+  name: "AllBusiness",
+   props: ['currAdmin', 'allCompany', 'allBusiness', 'allFounders'],
+   methods: {
+    getAocBusiness(companyId) {
+      let singleBusiness = this.allBusiness.find((business) => business.id == companyId)
+      if (!singleBusiness) {
+        return "Business Registration Not Found"
+      }
+      return singleBusiness
+    },
+    registrationCompletion(business, founders) {
+      let businessReg = business
+      let individualReg = founders
+      if (businessReg && individualReg) {
+
+        return { width: "100%", percentage: 100, color: "green" }
+      }
+      if (businessReg || individualReg) {
+
+        return { width: "54%", percentage: 54, color: "blue" }
+      }
+      return { width: "12%", percentage: 12, color: "red" }
+    },
+
+    registrationBadge(business, founders) {
+      let businessReg = business
+      let individualReg = founders
+      if (businessReg && individualReg) {
+        return { badge: "badge-success", status: "Completed" }
+      }
+      return { badge: "badge-warning", status: "Pending" }
+    },
+
+
+    companyFounders(businessId) {
+      let founders = this.allFounders.filter((e) => {
+        return e.businessId == businessId
+      })
+      let widthPercentage = `${founders.length * 10}%`;
+      return { founders: founders, length: founders.length, width: widthPercentage }
+
+    }
+  },
+  computed: {
+    numberOfCompany() {
+      return this.allCompany.length
+    },
+    numberOfBusinessRegistered() {
+      return this.allBusiness.length
+    },
+    numberOfFounders() {
+      return this.allFounders.length
+    },
+    numberOfUnregisteredBusiness() {
+      let unregBusiness = this.allCompany.filter((e) => {
+        return e.businessRegistration == false
+      })
+      return unregBusiness.length
+    },
+
+    businessSectors() {
+      let sector = this.allBusiness.filter((e) => {
+        return e.businessSectors
+      })
+      return sector
+    }
+  }
 
 }
 </script>
--- src/components/Admin/index.vue
+++ src/components/Admin/index.vue
@@ -156,11 +156,13 @@
                                     </h5>
                                   </td>
                                 </tr>
-                                <tr>
+                                <tr v-if="company.founderRegistration">
                                   <td class="text-muted">Founders</td>
                                   <td class="w-100 px-0">
                                     <div class="founders">
-                                      <div class="each" v-for="(eachFounder, index) in  companyFounders(company.businessId).founders" :key="index">
+                                      <div class="each"
+                                        v-for="(eachFounder, index) in  companyFounders(company.businessId).founders"
+                                        :key="index">
                                         <v-avatar>
                                           <v-img :src="eachFounder.founderImage" alt="">
                                             <template #placeholder>
@@ -170,7 +172,7 @@
                                             </template>
                                           </v-img>
                                         </v-avatar>
-                                        <p>{{eachFounder.firstName}}</p>
+                                        <p>{{ eachFounder.firstName }}</p>
                                       </div>
 
                                     </div>
@@ -207,72 +209,26 @@
         <div class="col-md-12 grid-margin stretch-card">
           <div class="card">
             <div class="card-body">
-              <p class="card-title mb-0">Top Products</p>
+              <p class="card-title mb-0">All Company</p>
               <div class="table-responsive">
                 <table class="table table-striped table-borderless">
                   <thead>
                     <tr>
-                      <th>Product</th>
-                      <th>Price</th>
-                      <th>Date</th>
-                      <th>Status</th>
+                      <th>Business Name</th>
+                      <th>Business Sector</th>
+                      <th>Business Email</th>
+                      <th>Registration Status</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>Search Engine Marketing</td>
-                      <td class="font-weight-bold">$362</td>
-                      <td>21 Sep 2018</td>
+                    <tr v-for="(company, index) in allCompany" :key="index">
+                      <td>{{ company.companyName }}</td>
+                      <td class="font-weight-bold">{{ getAocBusiness(company.id).businessSector }}</td>
+                      <td>{{ company.companyEmail }}</td>
                       <td class="font-weight-medium">
-                        <div class="badge badge-success">Completed</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Search Engine Optimization</td>
-                      <td class="font-weight-bold">$116</td>
-                      <td>13 Jun 2018</td>
-                      <td class="font-weight-medium">
-                        <div class="badge badge-success">Completed</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Display Advertising</td>
-                      <td class="font-weight-bold">$551</td>
-                      <td>28 Sep 2018</td>
-                      <td class="font-weight-medium">
-                        <div class="badge badge-warning">Pending</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Pay Per Click Advertising</td>
-                      <td class="font-weight-bold">$523</td>
-                      <td>30 Jun 2018</td>
-                      <td class="font-weight-medium">
-                        <div class="badge badge-warning">Pending</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>E-Mail Marketing</td>
-                      <td class="font-weight-bold">$781</td>
-                      <td>01 Nov 2018</td>
-                      <td class="font-weight-medium">
-                        <div class="badge badge-danger">Cancelled</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Referral Marketing</td>
-                      <td class="font-weight-bold">$283</td>
-                      <td>20 Mar 2018</td>
-                      <td class="font-weight-medium">
-                        <div class="badge badge-warning">Pending</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Social media marketing</td>
-                      <td class="font-weight-bold">$897</td>
-                      <td>26 Oct 2018</td>
-                      <td class="font-weight-medium">
-                        <div class="badge badge-success">Completed</div>
+                        <div class="badge " :class="registrationBadge(company.businessRegistration, company.founderRegistration).badge">{{
+                            registrationBadge(company.businessRegistration, company.founderRegistration).status
+                        }}</div>
                       </td>
                     </tr>
                   </tbody>
@@ -283,41 +239,6 @@
         </div>
       </div>
       <!-- end of the copany data  -->
-
-
-      <!-- this is the advaced table side -->
-      <div class="row">
-        <div class="col-md-12 grid-margin stretch-card">
-          <div class="card">
-            <div class="card-body">
-              <p class="card-title">Advanced Table</p>
-              <div class="row">
-                <div class="col-12">
-                  <div class="table-responsive">
-                    <table id="example" class="display expandable-table" style="width:100%">
-                      <thead>
-                        <tr>
-                          <th>Quote#</th>
-                          <th>Product</th>
-                          <th>Business type</th>
-                          <th>Policy holder</th>
-                          <th>Premium</th>
-                          <th>Status</th>
-                          <th>Updated at</th>
-                          <th></th>
-                        </tr>
-                      </thead>
-                    </table>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-        </div>
-      </div>
-      <!-- end the advaced table side -->
     </div>
   </div>
 
@@ -353,6 +274,16 @@ export default {
       return { width: "12%", percentage: 12, color: "red" }
     },
 
+    registrationBadge(business, founders) {
+      let businessReg = business
+      let individualReg = founders
+      if (businessReg && individualReg) {
+        return { badge: "badge-success", status: "Completed" }
+      }
+      return { badge: "badge-warning", status: "Pending" }
+    },
+
+
     companyFounders(businessId) {
       let founders = this.allFounders.filter((e) => {
         return e.businessId == businessId
@@ -378,18 +309,29 @@ export default {
       })
       return unregBusiness.length
     },
-    // numberOfBusinessRegistered(){
-    //   let regBusiness = this.allCompany.filter((e)=>{
-    //     return e.businessRegistration == true
-    //   })
-    //   return regBusiness.length
-    // }
+
+    businessSectors() {
+      let sector = this.allBusiness.filter((e) => {
+        return e.businessSectors
+      })
+      return sector
+    }
   }
 }
 </script>
 
 <style scoped>
 .founders {
-  border: 1px solid red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+
+.each {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
